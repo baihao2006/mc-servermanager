43ae4ea8c82e682d2cb736109757becaaf9edb2a
Kashmir <169076356+baihao2006@users.noreply.github.com>
2025-12-17 00:45:39 +0800

    Add files via upload

---
+++ build/MCServerManager/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/a/_work/1/s/handheld/src-server
+# Build directory: D:/a/_work/1/s/build/win32_server_x64_previewapp_ninja/handheld/src-server
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
